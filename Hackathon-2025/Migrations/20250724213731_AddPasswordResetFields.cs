94c2c9e6e39929fde01d3b6dd84240d04581d9a7
Windeko <tylerjameswoody@gmail.com>
2025-07-24 14:43:54 -0700

    Added download and share feature

---
+++ Hackathon-2025/Migrations/20250724213731_AddPasswordResetFields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hackathon_2025.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetExpires",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetExpires",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "Users");
+        }
+    }
+}
