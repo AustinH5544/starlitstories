42ee0453e8b0d6b4a172d30d0d9f81c66f5c34b7
Windeko <tylerjameswoody@gmail.com>
2025-10-07 08:47:01 -0700

    Set the Vite base to /

--- Hackathon-2025/ClientApp/vite.config.js
+++ Hackathon-2025/ClientApp/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+    base: '/',
     plugins: [react()],
-    // no dev proxy needed when API_URL points to your backend
-});
+})
